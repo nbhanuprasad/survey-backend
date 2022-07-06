a2a0e905dc59f75055c79360a40dbbdee4fd9e0a
AnirudhNelli9 <anirudhvinay.nelli@eagles.oc.edu>
2022-07-06 13:10:07 -0500

    admin list completed

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,19 +1,17 @@
-module.exports = app => {
-  const lessons = require("../controllers/lesson.controller.js");
-  var router = require("express").Router();
-  // Create a new Lesson for a Tutorial
-  router.post("/:tutorialId/lessons/", lessons.create);
-  // Retrieve all Lessons for a Tutorial
-  router.get("/:tutorialId/lessons/", lessons.findAll);
-  // Retrieve all published Lessons for a Tutorial
-  router.get("/:tutorialId/lessons/published", lessons.findAllPublished);
-  // Retrieve a single Lesson with id
-  router.get("/:tutorialId/lessons/:id", lessons.findOne);
-  // Update a Lesson with id
-  router.put("/:tutorialId/lessons/:id", lessons.update);
-  // Delete a Lesson with id
-  router.delete("/:tutorialId/lessons/:id", lessons.delete);
-  // Delete all Lessons
-  router.delete("/:tutorialId/lessons/:id", lessons.deleteAll);
-  app.use('/api/tutorials', router);
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get("/api/superadmin/alladmins", [authJwt.verifyToken, authJwt.isSuperAdmin], controller.getAllAdmins);
+
+ 
+
+
 };
