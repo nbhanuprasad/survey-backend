7bddc0774f15accc6a5169a157e2e75dec93bb45
nbhanuprasad <nbhanuprasad@gmail.com>
2022-07-10 05:44:07 -0500

    admin login

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -35,7 +35,7 @@ exports.signin = (req, res) => {
   })
     .then(user => {
       if (!user) {
-        return res.status(404).send({ message: "User Not found." });
+        return res.status(404).send({ message: "No User found." });
       }
 
       var passwordIsValid = bcrypt.compareSync(
