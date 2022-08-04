07d9213bf3d45b1331e21d753674d10710f9d02e
nbhanuprasad <nbhanuprasad@gmail.com>
2022-08-04 08:33:02 -0500

    update questoin

--- app/utils/sendEmail.js
+++ app/utils/sendEmail.js
@@ -12,7 +12,7 @@ exports.sendSurveyLinkEmail = async (userEmail, surveyLink) => {
         pass: 'ABC@abc1234',
         clientId: '350533973281-gpjq3g2mvnqv9fur910g98ja4nfalkpb.apps.googleusercontent.com',
         clientSecret: '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',
-        refreshToken: '1//04Wr-Nu125TxDCgYIARAAGAQSNwF-L9Ir-xNFf4esxGiVK7zjX2P-oaFQd9QqanWaT3Xh1q2Yejd9XYRXcbf92GLuAMu9M2mLY5A'
+        refreshToken: '1//04NSxYbs-2HFhCgYIARAAGAQSNwF-L9IrzXX0G1Rlnea1jF30WbfPMPiTA5A4ydgwMgu62fS9VaRTdIdmQNm5p54fPN871YbHnjY'
       }
     });
 
